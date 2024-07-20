7b4101864c6e95428412672c51116af6ff0bd37f
yawar-00 <yawar7999@gmail.com>
2024-07-20 16:43:56 +0530

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -446,18 +446,52 @@
 // }
 // export default App
 
+// import { useState } from "react";
+// function App(){
+//   const [name,setName]=useState("")
+//   const [city,setCity]=useState("")
+//   const handleSubmit=()=>{
+//     console.log({name:name, city:city});
+//   }
+//   return(
+//     <>
+//       Enter name : <input type="text" vlaue={name} onChange={(e)=>{setName(e.target.value)}} />
+//       <br />
+//       Enter city : <input type="text" vlaue={city} onChange={(e)=>{setCity(e.target.value)}} />
+//       <br />
+//       <button onClick={handleSubmit}>Submit</button>
+//     </>
+//   )
+// }
+// export default App;
+
 import { useState } from "react";
+import axios from "axios";
 function App(){
-  const [name,setName]=useState("")
-  const [city,setCity]=useState("")
+  const [input,setInput]=useState({})
+  
+  const handleInput=(e)=>{
+    let name =e.target.name;
+    let value=e.target.value;
+    setInput((values)=>({...values ,[name]:value}))
+    console.log(input);
+  }
   const handleSubmit=()=>{
-    console.log({name:name, city:city});
+    let api="http://localhost:3000/student"
+    axios.post(api, input).then((res)=>{
+      console.log(res);
+      alert("data save !!")
+    })
   }
   return(
     <>
-      Enter name : <input type="text" vlaue={name} onChange={(e)=>{setName(e.target.value)}} />
+      Enter rollno : <input type="text" vlaue={input.rollno} name="rollno" onChange={handleInput} />
       <br />
-      Enter city : <input type="text" vlaue={city} onChange={(e)=>{setCity(e.target.value)}} />
+      Enter name : <input type="text" vlaue={input.name} name="name" onChange={handleInput} />
+      <br />
+      Enter city : <input type="text" vlaue={input.city} name="city" onChange={handleInput} />
+      <br />
+      Enter fees : <input type="text" vlaue={input.fees} name="fees" onChange={handleInput} />
       <br />
       <button onClick={handleSubmit}>Submit</button>
     </>
